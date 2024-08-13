cf32ebf4dc027395023dcf3db8d522443104f3ae
mundane <799699348@qq.com>
2024-08-13 20:13:49 +0800

    删除一些无用log

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -3,9 +3,7 @@ import { booksService } from "@/services/books";
 
 export async function GET(request: NextRequest) {
   const token = request.cookies.get("Admin-Token")?.value;
-  console.log("token = ", token);
   const res = await booksService(token);
-  console.log("res = ", res);
   if (res.code !== 200) {
     return NextResponse.json({ code: res.code, message: res.msg });
   }
